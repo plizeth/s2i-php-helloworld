d7076ef8b177cb3c2eec59572cda02cbf6e5b229
silvinux <sperezto@sperezto.remote.csb>
2019-03-15 16:26:55 +0100

    first commit

--- includes/config.php
+++ includes/config.php
@@ -8,18 +8,7 @@
 function config($key = '')
 {
     $hostname = getenv('HOSTNAME');	
-    function curPageURL() {
-      $pageURL = 'http';
-      if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-      $pageURL .= "://";
-      if ($_SERVER["SERVER_PORT"] != "80") {
-       $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-      } else {
-       $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-      }
-      return $pageURL;
-     }
-    $url = curPageURL();
+    $url = $_SERVER["HTTP_HOST"];
     $config = [
         'name' => $hostname,
         'site_url' => $url,
